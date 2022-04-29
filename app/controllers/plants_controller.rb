288e7e3a06eb85dee1471c9807f39df56b84b623
Jasmine Elkins <jasmine.elkins@gmail.com>
2022-04-29 16:04:12 -0400

    commit tree a4de7fdb071e22ea8994fc7daab40b3e4a8e2f78

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,4 @@
 class PlantsController < ApplicationController
-
   # GET /plants
   def index
     plants = Plant.all
@@ -8,19 +7,37 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
   # POST /plants
   def create
-    plant = Plant.create(plant_params)
+    plant = Plant.create!(plant_params)
     render json: plant, status: :created
+  end
+
+  # UPDATE /plants/:id
+  def update
+    plant = find_plant
+    plant.update!(plant_params)
+    render json: plant
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    plant = find_plant
+    plant.destroy
+    render json: {}
   end
 
   private
 
   def plant_params
-    params.permit(:name, :image, :price, :is_in_stock)
+    params.permit(:name, :image, :id, :is_in_stock, :price)
+  end
+
+  def find_plant
+    Plant.find_by!(id: params[:id])
   end
 end
